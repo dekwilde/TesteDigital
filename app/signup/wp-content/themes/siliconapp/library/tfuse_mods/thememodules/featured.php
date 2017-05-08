a4544329fe00427cf3ea0133aae5c0231c6a2079
DekWilde <dekwilde@gmail.com>
2017-05-08 14:55:09 -0300

    cleaning project

--- app/signup/wp-content/themes/siliconapp/library/tfuse_mods/thememodules/featured.php
+++
@@ -1,6 +0,0 @@
-<div class="options sec_pag_options"> 
-	<?php echo tfuse_qtranslate(get_option(PREFIX.'_middle_box_1')) ?>
-	<?php echo tfuse_qtranslate(get_option(PREFIX.'_middle_box_2')) ?>
-	<?php echo tfuse_qtranslate(get_option(PREFIX.'_middle_box_3')) ?>
-	<div class="clear_container"></div>
-</div>
--- app/signup/wp-content/themes/siliconapp/library/tfuse_mods/thememodules/page-nav-footer.php
+++
@@ -1,3 +0,0 @@
-<?php
-wp_nav_menu( array('depth' => 1, 'container_class' => '', 'menu_class' => 'footer_links', 'menu_id' => '', 'fallback_cb' => 'default_menu', 'theme_location' => 'bottom' ) );
-?>
